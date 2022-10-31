63261f3af8feb6e322d7246b9c3d825e88546a38
Mytiaiev <mytiaievyevhen@gmail.com>
2022-10-31 16:29:35 +0200

    add landing page as root, fix html bugs

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.views.generic import TemplateView
 from scripts.driversrating import DriversRatingMixin
-from django.shortcuts import render 
+from django.shortcuts import render
 
 
 
