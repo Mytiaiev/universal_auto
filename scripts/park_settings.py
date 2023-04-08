7fdd3ecb8ebce8196c0212a13f2bd8731662961a
Ihor Sivak <igorsivak97@gmail.com>
2023-04-08 17:02:23 +0300

    park_settings script

---
+++ scripts/park_settings.py
@@ -0,0 +1,27 @@
+from app.models import ParkSettings
+
+
+default = '0'
+settings = {
+    'FREE_RENT': default,
+    'RENT_PRICE': default,
+    'TARIFF_IN_THE_CITY:': '15',
+    'TARIFF_OUTSIDE_THE_CITY:': '30'
+}
+
+
+def init_park_settings():
+    for key, value in settings.items():
+        park_setting = ParkSettings(
+            key=key,
+            value=value)
+        try:
+            park_setting.save()
+        except IntegrityError:
+            pass
+
+
+def run():
+    init_park_settings()
+
+
