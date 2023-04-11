df746296b841a045199b6c452a7a91601498d1c8
Ihor Sivak <igorsivak97@gmail.com>
2023-04-11 17:24:42 +0300

    get address by radius

--- scripts/conversion.py
+++ scripts/conversion.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import re
 
 
 def get_address(latitude, longitude, api_key):
@@ -35,4 +36,28 @@ def geocode(address, api_key):
         return latitude, longitude
     else:
         return None
+
+
+def get_addresses_by_radius(address, center_lat, center_lng, center_radius, api_key):
+
+    url = f"https://maps.googleapis.com/maps/api/place/autocomplete/json?input={address}&language=uk&" \
+        f"location={center_lat},{center_lng}&radius={center_radius}&key={api_key}"
+
+    response = requests.get(url)
+    data = response.json()
+    city_park = os.environ["CITY_PARK"]
+    addresses = []
+    pattern = re.compile(rf"\b{city_park}\b", re.IGNORECASE)
+
+    if data['status'] == 'OK':
+        results = data['predictions']
+        for result in results:
+            match = pattern.search(result['description'])
+            if match:
+                addresses.append(result['description'])
+    else:
+        return None
+
+    return addresses
+
 
