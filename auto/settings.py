cbd9ea73f0facb02a9a4d46755eabbb2ea6b8367
Ihor Sivak <igorsivak97@gmail.com>
2023-04-26 13:52:21 +0300

    added db-retry to middleware

--- auto/settings.py
+++ auto/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django_db_conn_retry.middleware.DBConnRetryMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
