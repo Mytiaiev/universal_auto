faf46ac2fdf4e5e445d8e99b4ccf95d3a889e72d
Ihor Sivak <igorsivak97@gmail.com>
2023-05-18 10:21:54 +0300

    upd

--- auto_bot/handlers/comment/handlers.py
+++ auto_bot/handlers/comment/handlers.py
@@ -15,24 +15,27 @@ def comment(update, context):
     except:
         chat_id = update.message.chat.id
     user = User.get_by_chat_id(chat_id)
-    order = Order.get_order(chat_id_client=chat_id,
-                            phone=user.phone_number,
-                            status_order=Order.WAITING)
+    if user:
+        order = Order.get_order(chat_id_client=chat_id,
+                                phone=user.phone_number,
+                                status_order=Order.WAITING)
 
-    s_order = Order.objects.filter(chat_id_client=chat_id,
-                                   phone_number=user.phone_number,
-                                   status_order__in=[Order.IN_PROGRESS, Order.COMPLETED]).last()
-    if any((order, s_order)):
-        if order:
-            order.status_order = Order.CANCELED
-            order.save()
-            update.message.reply_text('Поставте оцінку або напишіть відгук',
-                                      reply_markup=markup_keyboard_onetime(comment_keyboard))
+        s_order = Order.objects.filter(chat_id_client=chat_id,
+                                       phone_number=user.phone_number,
+                                       status_order__in=[Order.IN_PROGRESS, Order.COMPLETED]).last()
+        if any((order, s_order)):
+            if order:
+                order.status_order = Order.CANCELED
+                order.save()
+                update.message.reply_text('Поставте оцінку або напишіть відгук',
+                                          reply_markup=markup_keyboard_onetime(comment_keyboard))
+            else:
+                context.bot.send_message(chat_id=chat_id, text='Поставте оцінку або напишіть відгук',
+                                         reply_markup=markup_keyboard_onetime(comment_keyboard))
         else:
-            context.bot.send_message(chat_id=chat_id, text='Поставте оцінку або напишіть відгук',
-                                     reply_markup=markup_keyboard_onetime(comment_keyboard))
+            update.message.reply_text('Залишіть відгук', reply_markup=ReplyKeyboardRemove())
     else:
-        update.message.reply_text('Залишіть відгук', reply_markup=ReplyKeyboardRemove())
+        update.message.reply_text('Щоб залишити відгук надайте номер телефону')
 
 
 def save_comment(update, context):
@@ -46,4 +49,4 @@ def save_comment(update, context):
         order.comment = user_comment
         order.save()
     context.user_data['state'] = None
-    update.message.reply_text("Ваш відгук було збережено. Очікуйте, менеджер скоро з вами зв`яжеться!")
+    update.message.reply_text("Ваш відгук було збережено")
