a1e3af8ffb2f579b4f60015533ee91906e5cc766
Ihor Sivak <igorsivak97@gmail.com>
2023-05-24 15:00:09 +0300

    add ninjapaymentsorder calculate and include to salary report

--- scripts/weekly.py
+++ scripts/weekly.py
@@ -3,10 +3,10 @@ from app.models import get_report
 
 def run(*args):
     if args:
-        week = f"2022W{args[0]}5"
+        week_number = f"2022W{args[0]}5"
     else:
-        week = None
-    print(get_report(week=False, week_number=week, driver=True, sleep=5, headless=True))
+        week_number = None
+    print(get_report(week=True, week_number=week_number, driver=True, sleep=5, headless=True))
 
 
 
