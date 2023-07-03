d365f9412c57e05c4bb86d44ce8bd4ab2c445c2c
Ihor Sivak <igorsivak97@gmail.com>
2023-07-03 10:41:51 +0300

    create and update methods for senc_v2

--- selenium_ninja/new_synchronizer.py
+++ selenium_ninja/new_synchronizer.py
@@ -51,20 +51,21 @@ class Synchronizer_V2:
             self.create_driver(**driver)
 
     def create_driver(self, **kwargs):
+        pay_cash, id, fleet_ = 'pay_cash', 'driver_external_id', 'fleet_name'
         try:
-            fleet = Fleet.objects.get(name=kwargs['fleet_name'])
+            fleet = Fleet.objects.get(name=kwargs[fleet_])
         except Fleet.DoesNotExist:
             return
         drivers = Fleets_drivers_vehicles_rate.objects.filter(fleet=fleet,
-                                                              driver_external_id=kwargs['driver_external_id'],
+                                                              driver_external_id=kwargs[id],
                                                               partner=self.get_partner())
         if not drivers:
             fleets_drivers_vehicles_rate = Fleets_drivers_vehicles_rate.objects.create(
                 fleet=fleet,
                 driver=self.get_or_create_driver(**kwargs),
                 vehicle=self.get_or_create_vehicle(**kwargs),
-                driver_external_id=kwargs['driver_external_id'],
-                pay_cash=kwargs['pay_cash'],
+                driver_external_id=kwargs[id],
+                pay_cash=kwargs[pay_cash],
                 partner=self.get_partner(),
             )
             fleets_drivers_vehicles_rate.save()
@@ -72,17 +73,17 @@ class Synchronizer_V2:
             self.update_vehicle_fields(fleets_drivers_vehicles_rate.vehicle, **kwargs)
         else:
             for fleets_drivers_vehicles_rate in drivers:
-                if fleets_drivers_vehicles_rate.pay_cash != kwargs['pay_cash']:
-                    fleets_drivers_vehicles_rate.pay_cash = kwargs['pay_cash']
-                    fleets_drivers_vehicles_rate.save(update_fields=['pay_cash'])
+                if fleets_drivers_vehicles_rate.pay_cash != kwargs[pay_cash]:
+                    fleets_drivers_vehicles_rate.pay_cash = kwargs[pay_cash]
+                    fleets_drivers_vehicles_rate.save(update_fields=[pay_cash])
                 self.update_driver_fields(fleets_drivers_vehicles_rate.driver, **kwargs)
                 self.update_vehicle_fields(fleets_drivers_vehicles_rate.vehicle, **kwargs)
 
-    def update_driver_fields(self, driver, **kwargs): #+
+    def update_driver_fields(self, driver, **kwargs):
         key_phone, key_email = 'phone_number', 'email'
         update_fields, phone_number, email = [], kwargs[key_phone], kwargs[key_email]
 
-        if driver.phone_number != phone_number and phone_number:
+        if not driver.phone_number and phone_number:
             driver.phone_number = phone_number
             update_fields.append(key_phone)
         elif driver.email != email and email:
@@ -91,7 +92,7 @@ class Synchronizer_V2:
         elif update_fields:
             driver.save(update_fields=update_fields)
 
-    def update_vehicle_fields(self, vehicle, **kwargs): #+
+    def update_vehicle_fields(self, vehicle, **kwargs):
         key_vehicle_name, key_vin = 'vehicle_name',  'vin_code'
         update_fields, vehicle_name, vin_code = [], kwargs[key_vehicle_name], kwargs[key_vin]
 
@@ -104,95 +105,40 @@ class Synchronizer_V2:
         elif update_fields:
             vehicle.save(update_fields=update_fields)
 
-
-    def get_or_create_vehicle(self, **kwargs): #+
-        licence_plate = kwargs['licence_plate']
+    def get_or_create_vehicle(self, **kwargs):
+        licence_plate, unk, v_name, vin = kwargs['licence_plate'], 'Unknown car', 'vehicle_name', 'vin_code'
         if not licence_plate:
-            licence_plate = 'Unknown car'
+            licence_plate = unk
         try:
             vehicle = Vehicle.objects.get(licence_plate=licence_plate, partner=self.get_partner())
         except Vehicle.DoesNotExist:
             vehicle = Vehicle.objects.create(
-                name=kwargs['vehicle_name'].upper(),
+                name=kwargs[v_name].upper(),
                 model='',
                 type='',
                 licence_plate=licence_plate,
-                vin_code=kwargs['vin_code'],
+                vin_code=kwargs[vin],
                 partner=self.get_partner()
             )
             vehicle.save()
         return vehicle
 
-    def get_driver_by_name(self, name, second_name): #+
-        try:
-            return Driver.objects.get(name=name, second_name=second_name, partner=self.get_partner())
-        except Driver.MultipleObjectsReturned:
-            return Driver.objects.filter(name=name, second_name=second_name, partner=self.get_partner())[0]
-
-    def get_driver_by_phone_or_email(self, phone_number, email):
-        try:
-            if len(phone_number):
-                return Driver.objects.get(phone_number__icontains=phone_number[-10::])
-            else:
-                raise Driver.DoesNotExist
-        except (Driver.MultipleObjectsReturned, Driver.DoesNotExist):
-            try:
-                return Driver.objects.get(email__icontains=email)
-            except Driver.MultipleObjectsReturned:
-                raise Driver.DoesNotExist
-
     def get_or_create_driver(self, **kwargs):
+        name, s_name, phone, email = 'name', 'second_name', 'phone_number', 'email'
         try:
-            driver = self.get_driver_by_name(kwargs['name'], kwargs['second_name'])
+            driver = Driver.objects.get(name=kwargs[name],
+                                        second_name=kwargs[s_name],
+                                        partner=self.get_partner())
         except Driver.DoesNotExist:
-            try:
-                driver = self.get_driver_by_name(kwargs['second_name'], kwargs['name'])
-            except Driver.DoesNotExist:
-                t_name, t_second_name = self.split_name(
-                    self.translate_text(f'{kwargs["name"]} {kwargs["second_name"]}', 'uk'))
-                try:
-                    driver = self.get_driver_by_name(t_name, t_second_name)
-                except Driver.DoesNotExist:
-                    try:
-                        driver = self.get_driver_by_name(t_second_name, t_name)
-                    except Driver.DoesNotExist:
-                        t_name, t_second_name = self.split_name(
-                            self.translate_text(f'{kwargs["name"]} {kwargs["second_name"]}', 'ru'))
-                        try:
-                            driver = self.get_driver_by_name(t_name, t_second_name)
-                        except Driver.DoesNotExist:
-                            try:
-                                driver = self.get_driver_by_name(t_second_name, t_name)
-                            except Driver.DoesNotExist:
-                                try:
-                                    driver = self.get_driver_by_phone_or_email(kwargs['phone_number'], kwargs['email'])
-                                except Driver.DoesNotExist:
-                                    driver = Driver.objects.create(
-                                        name=kwargs['name'],
-                                        second_name=kwargs['second_name'],
-                                        phone_number=kwargs['phone_number'],
-                                        email=kwargs['email']
-                                    )
-                                    driver.save()
+            driver = Driver.objects.create(
+                name=kwargs[name],
+                second_name=kwargs[s_name],
+                phone_number=kwargs[phone],
+                email=kwargs[email],
+                partner=self.get_partner()
+            )
+            driver.save()
         return driver
-
-    def translate_text(self, text, to_lang):
-        try:
-            return tss.google(text, to_language=to_lang, if_use_cn_host=False)
-        except Exception:
-            return text
-
-    def split_name(self, name):
-        name_list = [x for x in name.split(' ') if len(x) > 0]
-        name = ''
-        second_name = ''
-        try:
-            name = name_list[0]
-            second_name = name_list[1]
-        except IndexError:
-            pass
-        res = (name, second_name)
-        return res
 
     def validate_phone_number(self, phone_number):
         return ''.join([x for x in phone_number if x.isdigit() or x == '+'][:13])
