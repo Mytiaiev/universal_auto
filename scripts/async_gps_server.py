6c968ec8a4d8e76cd410a6ac345191c40d24a3c6
Родион <kurochkar001@gmail,com>
2023-07-25 21:52:36 +0300

    make gps without orm

--- scripts/async_gps_server.py
+++ scripts/async_gps_server.py
@@ -1,7 +1,8 @@
 import asyncio
 import logging
+import os
 import re
-
+import asyncpg
 from asgiref.sync import sync_to_async
 from auto.tasks import raw_gps_handler
 from django.db import connection
@@ -38,27 +39,32 @@ class PackageHandler:
     async def _d_handler(self, **kwargs):
         if self.imei and kwargs['msg']:
             imei,  client_ip, client_port = self.imei, kwargs['addr'][0], kwargs['addr'][1]
-            data, created_at = kwargs['msg'], now()
+            data, created_at = kwargs['msg'], await sync_to_async(now)()
             try:
-                async with sync_to_async(connection.cursor)() as cursor:
-                    query = """
-                                INSERT INTO app_rawgps (imei, client_ip, client_port, data, created_at)
-                                VALUES (%s, %s, %s, %s, %s)
-                            """
-                    params = (imei, client_ip, client_port, data, created_at)
-                    await sync_to_async(cursor.execute)(query, params)
-
-                    await sync_to_async(connection.commit)()
-                    obj_id = await sync_to_async(cursor.lastrowid)()
-
+                database_url = os.environ.get('DATABASE_URL')
+                conn = await asyncpg.connect(dsn=database_url)
+                query = """
+                            INSERT INTO app_rawgps (imei, client_ip, client_port, data, created_at)
+                            VALUES ($1, $2, $3, $4, $5)
+                        """
+                await conn.execute(query, imei, client_ip, client_port, data, created_at)
+                obj_id = await conn.fetchval("SELECT lastval();")
+                obj_id = int(obj_id)
+                await conn.close()
                 raw_gps_handler.delay(obj_id)
 
                 return self.answer_data
+            except (Exception, asyncpg.PostgresError) as error:
 
-            except connection.DatabaseError:
+                print("Error inserting GPS data:", error)
+
                 return self.answer_bad_data
         else:
             return self.answer_bad_data
+
+    @staticmethod
+    async def async_cursor():
+        return connection.cursor()
 
     async def _p_handler(self, **kwargs):
         return self.answer_ping
