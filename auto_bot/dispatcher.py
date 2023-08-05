63b2b4c0cfd6e552fdec87d8e205bab36c9bfb37
Родион <kurochkar001@gmail,com>
2023-08-05 15:15:31 +0300

    empty string in debug

--- auto_bot/dispatcher.py
+++ auto_bot/dispatcher.py
@@ -222,7 +222,7 @@ def setup_dispatcher(dp):
     dp.add_handler(job_docs_conversation)
     dp.add_handler(CommandHandler("cancel", cancel))
     dp.add_handler(MessageHandler(Filters.text, text))
-    dp.add_handler(error_handler)
+    dp.add_error_handler(error_handler)
     #
     # # need fix
     # dp.add_handler(CommandHandler('update', update_db, run_async=True))
