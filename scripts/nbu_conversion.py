95cbc79c3be722927b3ecc83f30644bdc6f0f9da
Ihor Sivak <igorsivak97@gmail.com>
2023-08-17 12:10:16 +0300

    update model vehicle and create conversation with help nbu

---
+++ scripts/nbu_conversion.py
@@ -0,0 +1,24 @@
+import requests
+from decimal import Decimal, ROUND_HALF_UP
+
+
+def get_exchange_rate(currency_code):
+    api_url = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={currency_code}&json'
+
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        data = response.json()
+        if data:
+            return data[0]['rate']
+    return None
+
+
+def convert_to_currency(amount_uah, to_currency):
+    usd_rate = get_exchange_rate(to_currency)
+
+    if usd_rate is not None:
+        amount_usd = amount_uah / usd_rate
+        rounded_value = Decimal(str(amount_usd)).quantize(Decimal('0.00'), rounding=ROUND_HALF_UP)
+        return rounded_value
+    return None
