5c4e0e7f8ca6c14d331acf85c06827c8c9dd21f0
OlegBondarUA <Petrovichteh@gmail.com>
2023-10-20 15:58:43 +0300

    add auto-park page and add one block

--- taxi_service/views.py
+++ taxi_service/views.py
@@ -21,7 +21,7 @@ from auto_bot.main import bot
 
 
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = "index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -34,15 +34,21 @@ class IndexView(TemplateView):
     def get_park_settings(self):
         park_settings = {}
         specific_keys = [
-            "FREE_CAR_SENDING_DISTANCE", "TARIFF_CAR_DISPATCH",
-            "TARIFF_CAR_OUTSIDE_DISPATCH", "TARIFF_IN_THE_CITY",
-            "TARIFF_OUTSIDE_THE_CITY", "CENTRE_CITY_LAT", "CENTRE_CITY_LNG",
-            "CENTRE_CITY_RADIUS", "SEND_TIME_ORDER_MIN", "SEARCH_TIME",
-            "MINIMUM_PRICE_RADIUS", "MAXIMUM_PRICE_RADIUS"
+            "FREE_CAR_SENDING_DISTANCE",
+            "TARIFF_CAR_DISPATCH",
+            "TARIFF_CAR_OUTSIDE_DISPATCH",
+            "TARIFF_IN_THE_CITY",
+            "TARIFF_OUTSIDE_THE_CITY",
+            "CENTRE_CITY_LAT",
+            "CENTRE_CITY_LNG",
+            "CENTRE_CITY_RADIUS",
+            "SEND_TIME_ORDER_MIN",
+            "SEARCH_TIME",
+            "MINIMUM_PRICE_RADIUS",
+            "MAXIMUM_PRICE_RADIUS",
         ]
 
-        park_setting_objects = ParkSettings.objects.filter(
-            key__in=specific_keys)
+        park_setting_objects = ParkSettings.objects.filter(key__in=specific_keys)
 
         for park_setting in park_setting_objects:
             park_settings[park_setting.key] = park_setting.value
@@ -56,30 +62,30 @@ class IndexView(TemplateView):
 class PostRequestView(View):
     def post(self, request):
         handler = PostRequestHandler()
-        action = request.POST.get('action')
+        action = request.POST.get("action")
 
         method = {
-            'order': handler.handler_order_form,
-            'subscribe': handler.handler_subscribe_form,
-            'send_comment': handler.handler_comment_form,
-            'order_sum': handler.handler_update_order,
-            'user_opt_out': handler.handler_update_order,
-            'increase_price': handler.handler_restarting_order,
-            'continue_search': handler.handler_restarting_order,
-            'uber': handler.handler_success_login,
-            'uber_logout': handler.handler_handler_logout,
-            'uklon': handler.handler_success_login,
-            'uklon_logout': handler.handler_handler_logout,
-            'bolt': handler.handler_success_login,
-            'bolt_logout': handler.handler_handler_logout,
-            'gps': handler.handler_success_login,
-            'gps_logout': handler.handler_handler_logout,
-            'login_invest': handler.handler_success_login_investor,
-            'logout_invest': handler.handler_logout_investor,
-            'change_password': handler.handler_change_password,
-            'send_reset_code': handler.handler_change_password,
-            'update_password': handler.handler_change_password,
-            'upd_database': handler.handler_update_database,
+            "order": handler.handler_order_form,
+            "subscribe": handler.handler_subscribe_form,
+            "send_comment": handler.handler_comment_form,
+            "order_sum": handler.handler_update_order,
+            "user_opt_out": handler.handler_update_order,
+            "increase_price": handler.handler_restarting_order,
+            "continue_search": handler.handler_restarting_order,
+            "uber": handler.handler_success_login,
+            "uber_logout": handler.handler_handler_logout,
+            "uklon": handler.handler_success_login,
+            "uklon_logout": handler.handler_handler_logout,
+            "bolt": handler.handler_success_login,
+            "bolt_logout": handler.handler_handler_logout,
+            "gps": handler.handler_success_login,
+            "gps_logout": handler.handler_handler_logout,
+            "login_invest": handler.handler_success_login_investor,
+            "logout_invest": handler.handler_logout_investor,
+            "change_password": handler.handler_change_password,
+            "send_reset_code": handler.handler_change_password,
+            "update_password": handler.handler_change_password,
+            "upd_database": handler.handler_update_database,
         }
 
         if action in method:
@@ -91,14 +97,14 @@ class PostRequestView(View):
 class GetRequestView(View):
     def get(self, request):
         handler = GetRequestHandler()
-        action = request.GET.get('action')
+        action = request.GET.get("action")
 
         method = {
-            'active_vehicles_locations': handler.handle_active_vehicles_locations,
-            'order_confirm': handler.handle_order_confirm,
-            'get_role': handler.handle_get_role,
-            'aggregators': handler.handle_check_aggregators,
-            'check_task': handler.handle_check_task,
+            "active_vehicles_locations": handler.handle_active_vehicles_locations,
+            "order_confirm": handler.handle_order_confirm,
+            "get_role": handler.handle_get_role,
+            "aggregators": handler.handle_check_aggregators,
+            "check_task": handler.handle_check_task,
         }
 
         if action in method:
@@ -107,37 +113,52 @@ class GetRequestView(View):
             return handler.handle_unknown_action(request)
 
 
+class AutoParkView(TemplateView):
+    template_name = "auto-park.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["subscribe_form"] = SubscriberForm()
+        return context
+
+
 class InvestmentView(View):
     def get(self, request):
-        return render(request, 'investment.html', {'subscribe_form': SubscriberForm()})
+        return render(request, "investment.html", {"subscribe_form": SubscriberForm()})
 
 
 class DriversView(TemplateView):
-    template_name = 'drivers.html'
+    template_name = "drivers.html"
     paginate_by = 8
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         drivers = Driver.objects.all()
         paginator = Paginator(drivers, self.paginate_by)
-        page_number = self.request.GET.get('page')
+        page_number = self.request.GET.get("page")
         page_obj = paginator.get_page(page_number)
-        context['subscribe_form'] = SubscriberForm()
-        context['page_obj'] = page_obj
+        context["subscribe_form"] = SubscriberForm()
+        context["page_obj"] = page_obj
         return context
 
 
 class DashboardView(TemplateView):
-    template_name = 'dashboard.html'
+    template_name = "dashboard.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['investor_group'] = self.request.user.groups.filter(name='Investor').exists()
-        context['partner_group'] = self.request.user.groups.filter(name='Partner').exists()
-        context['manager_group'] = self.request.user.groups.filter(name='Manager').exists()
+        context["investor_group"] = self.request.user.groups.filter(
+            name="Investor"
+        ).exists()
+        context["partner_group"] = self.request.user.groups.filter(
+            name="Partner"
+        ).exists()
+        context["manager_group"] = self.request.user.groups.filter(
+            name="Manager"
+        ).exists()
         # context['get_all_vehicle'] = Vehicle.objects.exclude(licence_plate='Unknown car')
         # context['average_effective_vehicle'] = average_effective_vehicle()
-        context['car_piggy_bank'] = CarsInformationListView.get_queryset(self)
+        context["car_piggy_bank"] = CarsInformationListView.get_queryset(self)
 
         return context
 
@@ -148,28 +169,26 @@ class GoogleAuthView(View):
         return super(GoogleAuthView, self).dispatch(request, *args, **kwargs)
 
     def post(self, request):
-        credential_data = request.POST.get('credential')
-        data = jwt.decode(credential_data, options={'verify_signature': False})
+        credential_data = request.POST.get("credential")
+        data = jwt.decode(credential_data, options={"verify_signature": False})
         email = data["email"].lower()
-        redirect_url = reverse('index')
+        redirect_url = reverse("index")
 
         if email:
             user = AuUser.objects.filter(email=email).first()
             if user:
-                user.backend = 'django.contrib.auth.backends.ModelBackend'
+                user.backend = "django.contrib.auth.backends.ModelBackend"
                 login(request, user)
-                redirect_url = reverse('dashboard')
+                redirect_url = reverse("dashboard")
             else:
-
-                return redirect(reverse('index') + "?signed_in=false")
+                return redirect(reverse("index") + "?signed_in=false")
 
         return HttpResponseRedirect(redirect_url)
 
 
 class SendToTelegramView(View):
     def get(self, request, *args, **kwargs):
-
-        chat_id = os.environ.get('TELEGRAM_BOT_CHAT_ID')
+        chat_id = os.environ.get("TELEGRAM_BOT_CHAT_ID")
 
         telegram_link = f"https://t.me/{bot.username}?start={chat_id}"
 
@@ -177,25 +196,25 @@ class SendToTelegramView(View):
 
 
 def blog(request):
-    return render(request, 'blog.html')
+    return render(request, "blog.html")
 
 
 def why(request):
-    return render(request, 'why.html', {'subscribe_form': SubscriberForm()})
+    return render(request, "why.html", {"subscribe_form": SubscriberForm()})
 
 
 def agreement(request):
-    return render(request, 'agreement.html', {'subscribe_form': SubscriberForm()})
+    return render(request, "agreement.html", {"subscribe_form": SubscriberForm()})
 
 
 class RobotsView(TemplateView):
-    template_name = 'robots.txt'
-    content_type = 'text/plain'
+    template_name = "robots.txt"
+    content_type = "text/plain"
 
 
 class SitemapView(TemplateView):
-    template_name = 'sitemap.xml'
-    content_type = 'application/xml'
+    template_name = "sitemap.xml"
+    content_type = "application/xml"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
